680d703177daf306ffd4526e2a8d3500cdc51e3f
周晓林 <zhouxiaolinux@163.com>
2018-03-23 12:36:43 +0800

    fix

--- hello.ts
+++ hello.ts
@@ -46,8 +46,6 @@ class Drawing2D {
     redraw() {
         let drawable = this.gpu.nextDrawable();
         let renderPassDescriptor = new WebGPURenderPassDescriptor();
-        // NOTE: Our API proposal has some of these values as declare enums, not constant numbers.
-        // We haven't got around to implementing the declare enums yet.
         renderPassDescriptor.colorAttachments[0].texture = drawable.texture;
         renderPassDescriptor.colorAttachments[0].loadAction = WebGPULoadAction.clear;
         renderPassDescriptor.colorAttachments[0].storeAction = WebGPUStoreAction.store;
@@ -59,10 +57,6 @@ class Drawing2D {
         let commandEncoder = commandBuffer.createRenderCommandEncoderWithDescriptor(renderPassDescriptor)
         commandEncoder.setRenderPipelineState(this.renderPipelineState);
         commandEncoder.setVertexBuffer(this.vertexBuffer,0,0)
-        // NOTE: We didn't attach any buffers. We create the geometry in the vertex shader using
-        // the vertex ID.
-
-        // NOTE: Our API proposal uses the declare enum value "triangle" here. We haven't got around to implementing the declare enums yet.
         commandEncoder.drawPrimitives(WebGPUPrimitiveType.triangle, 0, 3);
 
         commandEncoder.endEncoding();
